4905d8741b354d8c4337addd0d556684837a054f
umjoshua <59318184+umjoshua@users.noreply.github.com>
2021-09-22 11:34:09 +0530

    CPU Scheduling algo

--- CSL204 OS/fcfs_cpu.c
+++ CSL204 OS/fcfs_cpu.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(){
-    int bursturnaround[10]={0},arrival[10]={0},turnaround[10]={0},waiting[10]={0},completion[10]={0};
+    int burst[10]={0},arrival[10]={0},turnaround[10]={0},waiting[10]={0},completion[10]={0};
     int n,sum=0;
     float totalTAT=0,totalWT=0;
     printf("Enter number of processes: ");
@@ -13,10 +13,10 @@ int main(){
 	printf("Enter  burst time for each process:\n\n");
 		for(int i=0;i<n;i++){
 	printf("Burst time of process[%d]:",i+1);
-		scanf("%d",&bursturnaround[i]);
+		scanf("%d",&burst[i]);
 		}
     for(int j=0;j<n;j++){
-		sum+=bursturnaround[j];
+		sum+=burst[j];
 		completion[j]+=sum;
 	}
 	for(int k=0;k<n;k++){
@@ -24,13 +24,13 @@ int main(){
 		totalTAT+=turnaround[k];
 	}
 	for(int k=0;k<n;k++){
-		waiting[k]=turnaround[k]-bursturnaround[k];
+		waiting[k]=turnaround[k]-burst[k];
 		totalWT+=waiting[k];
 	}
 	printf("Process\t Arival Time\t Burst Time\t Completion Time\t Turn Arount Time\t Waiting Time\t\n\n");
 	
 	for(int i=0;i<n;i++){
-		printf("P%d\t\t %d\t\t %d\t\t %d\t\t %d\t\t %d\n",i+1,arrival[i],bursturnaround[i],completion[i],turnaround[i],waiting[i]);
+		printf("P%d\t\t %d\t\t %d\t\t %d\t\t %d\t\t %d\n",i+1,arrival[i],burst[i],completion[i],turnaround[i],waiting[i]);
 	}
 		
 	printf("\n\nAverage Turnaround Time = %f\n",totalTAT/n);
